3aad75d671cdc39a7943cc23591ed73d8e4def06
Sebastian009w <ia3266595@gmail.com>
2023-04-24 16:53:44 -0500

    creando tareas

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from .models import Task
 
+
 class CreateTask(ModelForm):
-    class meta:
+    class Meta:
         model = Task
         fields = ['title', 'description', 'important']
